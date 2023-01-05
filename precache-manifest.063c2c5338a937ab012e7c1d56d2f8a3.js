c84fbe46f57ae6e17fcccbb8ca49001436cb12cf
mrtcnnn <93640316+mrtcnnn@users.noreply.github.com>
2023-01-05 22:29:29 +0300

    Updates

---
+++ precache-manifest.063c2c5338a937ab012e7c1d56d2f8a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0b52e17d66d24ab469f",
+    "url": "/facerecognitionbrain/static/css/main.7fb2f318.chunk.css"
+  },
+  {
+    "revision": "f0b52e17d66d24ab469f",
+    "url": "/facerecognitionbrain/static/js/main.f0b52e17.chunk.js"
+  },
+  {
+    "revision": "a44c8584e1a06751c699",
+    "url": "/facerecognitionbrain/static/css/1.837e8e8d.chunk.css"
+  },
+  {
+    "revision": "a44c8584e1a06751c699",
+    "url": "/facerecognitionbrain/static/js/1.a44c8584.chunk.js"
+  },
+  {
+    "revision": "002c4e9bd45a266a78e3",
+    "url": "/facerecognitionbrain/static/js/runtime~main.002c4e9b.js"
+  },
+  {
+    "revision": "d87a82149714075ff51c5f0ff3c3daeb",
+    "url": "/facerecognitionbrain/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.bf35aa50bba96db90c07249ee6e7c281.js"
+  "/facerecognitionbrain/precache-manifest.063c2c5338a937ab012e7c1d56d2f8a3.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute("/facerecognitionbrain/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
